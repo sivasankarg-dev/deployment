faecc419be7c02162229f8681ee1696b7b843cf4
sivasankarg-dev <sivasankarg2605@gmail.com>
2026-09-11 16:24:12 +0530

    Database configuration and new endpoint added

---
+++ src/main/java/org/example/deploy/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.deploy.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String homePage(){
+        return "Welcome";
+    }
+}
--- src/main/java/org/example/deploy/services/UserService.java
+++ src/main/java/org/example/deploy/services/UserService.java
@@ -1,9 +1,11 @@
 package org.example.deploy.services;
 
 import org.example.deploy.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
     private UserRepository userRepository;
 }
